e5ab0a69404848f4f671e1098d69efeaf8fd3d0c
Vincent Lefoulon <vincent.lefoulon@free.fr>
2018-12-28 08:52:38 +0000

    Fix cron job

--- deploy/install.sh
+++ deploy/install.sh
@@ -29,12 +29,12 @@ sudo rm -rf Python-3.7.0*
 
 # Install Pipenv
 sudo pip3.7 install --upgrade pip
-sudo pip3.7 install pipenv gevent
+sudo pip3.7 install pipenv
 pipenv install
 
 # Run web server at startup
 crontab -r
-(crontab -l 2>/dev/null; echo "@reboot cd /home/pi/atelier && make prod") | crontab -
+(crontab -l 2>/dev/null; echo "@reboot cd /home/pi/atelier && PATH="'"$PATH:/usr/local/bin"'" && make prod") | crontab -
 
 echo "------------"
 echo "Installation finished."
--- deploy/update.sh
+++ deploy/update.sh
@@ -4,7 +4,6 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo pip3.7 install --upgrade pip pipenv
 
-cd ../atelier
 git pull
 pipenv update
 
