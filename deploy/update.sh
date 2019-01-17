bdd4254e7e3c58a1ae49cbe7f02f3a3121f4b19e
Vincent Lefoulon <vincent.lefoulon@free.fr>
2019-01-17 11:34:35 +0000

    Detect merge conflicts in update script

--- deploy/update.sh
+++ deploy/update.sh
@@ -7,9 +7,16 @@ sudo pip3.7 install --upgrade pip pipenv
 git stash
 git pull
 git stash apply
+STASH_RET=$?
 pipenv install
 
 echo "------------"
-echo "Update finished."
-echo "Please reboot to restart the server."
+if [ "$STASH_RET" -eq "0" ]; then
+    echo "Update finished."
+    echo "Please reboot to restart the server."
+else
+    echo "CONFLICTS DETECTED."
+    echo "Please solve them and add the changes to the git index."
+    echo "Then reboot to restart the server."
+fi
 echo "------------"
