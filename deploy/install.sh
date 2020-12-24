c87c9212291db17d4d41b8bb7db02720b916a3bd
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-12-24 18:41:03 +0100

    Install pyenv and poetry

--- deploy/install.sh
+++ deploy/install.sh
@@ -4,6 +4,17 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y sqlite3 build-essential libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev zlib1g libsqlite3-dev python3 python3-pip vim
 
+curl https://pyenv.run | bash
+exec $SHELL
+
+pyenv install 3.9.1
+cd ../atelier
+pyenv local 3.9.1
+cd ../deploy
+
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+exec $SHELL
+
 # Install Pipenv
 sudo pip3 install --upgrade pip
 sudo pip3 install pipenv
