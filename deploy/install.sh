7ea24f5f10cce4a12bd894daacc47c7947761639
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-12-24 19:24:27 +0100

    Use an intermediary bash file

--- deploy/install.sh
+++ deploy/install.sh
@@ -6,14 +6,20 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y sqlite3 build-essential libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev zlib1g libsqlite3-dev python3 vim
 
+# .bashrc is for interactive shells
+# If we update it here, we won't get our PATH updated
+# So we use an intermediary file
+echo >> ~/.bashrc
+echo 'source "$HOME/.bash_config"' >> ~/.bashrc
+
 echo
 echo "Installing pyenv..."
 curl https://pyenv.run | bash
-echo >> ~/.bashrc
-echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-. ~/.bashrc
+echo >> ~/.bash_config
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bash_config
+echo 'eval "$(pyenv init -)"' >> ~/.bash_config
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_config
+source ~/.bash_config
 
 echo
 echo "Setting local Python version..."
@@ -25,9 +31,9 @@ cd ../deploy
 echo
 echo "Installing poetry..."
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
-echo >> ~/.bashrc
-echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ~/.bashrc
-. ~/.bashrc
+echo >> ~/.bash_config
+echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ~/.bash_config
+source ~/.bash_config
 
 git config --global user.email "vincent.lefoulon@free.fr"
 git config --global user.name "Vincent Lefoulon"
@@ -39,7 +45,7 @@ poetry install
 
 # Run web server at startup
 crontab -r
-(crontab -l 2>/dev/null; echo "@reboot cd /home/pi/atelier && PATH="'"$PATH:/usr/local/bin"'" && make prod") | crontab -
+(crontab -l 2>/dev/null; echo "@reboot cd /home/pi/atelier && PATH="'"$HOME/.poetry/bin:$HOME/.pyenv/bin:$PATH:/usr/local/bin"'" && make prod") | crontab -
 
 echo "------------"
 echo "Installation finished."
