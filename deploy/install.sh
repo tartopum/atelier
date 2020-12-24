b9f9730b3540ff6662294d4d4dc081107104ba5d
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-12-24 19:32:59 +0100

    Run pyenv and poetry installation commands

--- deploy/install.sh
+++ deploy/install.sh
@@ -6,20 +6,19 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y sqlite3 build-essential libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev zlib1g libsqlite3-dev python3 vim
 
-# .bashrc is for interactive shells
-# If we update it here, we won't get our PATH updated
-# So we use an intermediary file
-echo >> ~/.bashrc
-echo 'source "$HOME/.bash_config"' >> ~/.bashrc
-
 echo
 echo "Installing pyenv..."
 curl https://pyenv.run | bash
-echo >> ~/.bash_config
-echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bash_config
-echo 'eval "$(pyenv init -)"' >> ~/.bash_config
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_config
-source ~/.bash_config
+echo >> ~/.bashrc
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
+# We need to run the commands since bashrc is for interactive envs and cannot
+# be sourced
+export PATH="$HOME/.pyenv/bin:$PATH"
+pyenv init -
+pyenv virtualenv-init -
 
 echo
 echo "Setting local Python version..."
@@ -31,9 +30,11 @@ cd ../deploy
 echo
 echo "Installing poetry..."
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
-echo >> ~/.bash_config
-echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ~/.bash_config
-source ~/.bash_config
+echo >> ~/.bashrc
+echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ~/.bashrc
+# We need to run the commands since bashrc is for interactive envs and cannot
+# be sourced
+export PATH="$HOME/.poetry/bin:$PATH"
 
 git config --global user.email "vincent.lefoulon@free.fr"
 git config --global user.name "Vincent Lefoulon"
