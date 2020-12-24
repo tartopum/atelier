7ca47360c548b8bfccc217871df54c59bf4e6b0f
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-12-24 18:37:12 +0100

    Install Debian Python

--- deploy/install.sh
+++ deploy/install.sh
@@ -2,34 +2,11 @@
 
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get install -y sqlite3 build-essential libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev zlib1g libsqlite3-dev vim
-
-# Install Python 3.7
-python3.7 -V
-if [ $? -ne 0 ];
-then
-    PY_DIR="Python-3.7.0"
-    if [ ! -d "$PY_DIR" ]; then
-        wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tgz
-        tar -xzvf Python-3.7.0.tgz
-    fi
-    cd $PY_DIR
-    ./configure
-    make
-    sudo make install
-fi
-
-python3.7 -V
-if [ $? -ne 0 ];
-then
-    exit 1
-fi
-
-sudo rm -rf Python-3.7.0*
+sudo apt-get install -y sqlite3 build-essential libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev zlib1g libsqlite3-dev python3 python3-pip vim
 
 # Install Pipenv
-sudo pip3.7 install --upgrade pip
-sudo pip3.7 install pipenv
+sudo pip3 install --upgrade pip
+sudo pip3 install pipenv
 pipenv install
 
 git config --global user.email "you@example.com"
