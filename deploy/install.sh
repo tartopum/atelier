6b4f6f77ef10b93f09b30c7f4d6f320a63094848
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-12-24 19:54:13 +0100

    Do not install pyenv anymore

--- deploy/install.sh
+++ deploy/install.sh
@@ -7,27 +7,6 @@ sudo apt-get -y upgrade
 sudo apt-get install -y sqlite3 build-essential libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev zlib1g libsqlite3-dev python3 vim
 
 echo
-echo "Installing pyenv..."
-curl https://pyenv.run | bash
-echo >> ~/.bashrc
-echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-
-# We need to run the commands since bashrc is for interactive envs and cannot
-# be sourced
-export PATH="$HOME/.pyenv/bin:$PATH"
-pyenv init -
-pyenv virtualenv-init -
-
-echo
-echo "Setting local Python version..."
-pyenv install 3.9.1
-cd ../atelier
-pyenv local 3.9.1
-cd ../deploy
-
-echo
 echo "Installing poetry..."
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 echo >> ~/.bashrc
@@ -42,11 +21,12 @@ git config --global user.name "Vincent Lefoulon"
 python3 config.py ../atelier/config.json
 
 cd ../atelier
+poetry env use python3
 poetry install
 
 # Run web server at startup
 crontab -r
-(crontab -l 2>/dev/null; echo "@reboot cd /home/pi/atelier && PATH="'"$HOME/.poetry/bin:$HOME/.pyenv/bin:$PATH:/usr/local/bin"'" && make prod") | crontab -
+(crontab -l 2>/dev/null; echo "@reboot cd /home/pi/atelier && PATH="'"$HOME/.poetry/bin:$PATH:/usr/local/bin"'" && make prod") | crontab -
 
 echo "------------"
 echo "Installation finished."
