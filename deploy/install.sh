201e96359a7208f7a479ae24ee22600e64eb44aa
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-12-24 18:46:56 +0100

    Add pyenv to PATH

--- deploy/install.sh
+++ deploy/install.sh
@@ -5,6 +5,10 @@ sudo apt-get -y upgrade
 sudo apt-get install -y sqlite3 build-essential libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev zlib1g libsqlite3-dev python3 python3-pip vim
 
 curl https://pyenv.run | bash
+echo >> ~/.bashrc
+echo 'export PATH="/home/pi/.pyenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 exec $SHELL
 
 pyenv install 3.9.1
