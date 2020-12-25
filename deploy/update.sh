5ea3a698259acb8b92d5f99dd03c3c26aae18113
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-12-25 10:33:17 +0100

    Add poetry to PATH in update script

--- deploy/update.sh
+++ deploy/update.sh
@@ -2,15 +2,25 @@
 
 set -eu
 
+echo
+echo "Updating Debian packages..."
 sudo apt-get update
 sudo apt-get -y upgrade
 
+echo
+echo "Pulling git repo..."
 git stash
 git pull
 git stash apply
 STASH_RET=$?
+
+echo
+echo "Updating Python dependencies..."
+export PATH="$HOME/.poetry/bin:$PATH"
 poetry install
 
+echo
+echo
 echo "------------"
 if [ "$STASH_RET" -eq "0" ]; then
     echo "Update finished."
