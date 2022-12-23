a5946e07a7ca816a6ceb3afb9d3f8743a44f4d59
Vincent Lefoulon <vincent.lefoulon@free.fr>
2022-12-23 13:08:11 +0100

    Fix poetry installation

--- deploy/install.sh
+++ deploy/install.sh
@@ -11,6 +11,14 @@ sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
 echo
 echo "Installing pyenv..."
 curl https://pyenv.run | bash
+echo >> ~/.profile
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init -)"' >> ~/.profile
+echo >> ~/.bashrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 
 echo
 echo "Installing Python 3.11..."
@@ -18,12 +26,14 @@ pyenv install 3.11
 
 echo
 echo "Installing poetry..."
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+curl -sSL https://install.python-poetry.org | python3 -
+echo >> ~/.profile
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.profile
 echo >> ~/.bashrc
-echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ~/.bashrc
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
 # We need to run the commands since bashrc is for interactive envs and cannot
 # be sourced
-export PATH="$HOME/.poetry/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
 
 echo
 echo "Installing Python dependencies..."
